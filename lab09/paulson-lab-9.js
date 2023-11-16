1e782a7aef8b5dbf1c7a76b9876c9e7fb72e6490
Kynedi <Kynedi.Paulson@trojans.dsu.edu>
2023-11-16 10:49:35 -0700

    11/16

--- lab09/paulson-lab-9.js
+++ lab09/paulson-lab-9.js
@@ -3,10 +3,9 @@ const emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.edu$/;
 const datePattern = /^((0[13578]|1[02])\/31\/(18|19|20)[0-9]{2})|((01|0[3-9]|1[0-2])\/(29|30)\/(18|19|20)[0-9]{2})|((0[1-9]|1[0-2])\/(0[1-9]|1[0-9]|2[0-8])\/(18|19|20)[0-9]{2})|((02)\/29\/(((18|19|20)(04|08|[2468][048]|[13579][26]))|2000))$/;
 const phonePattern = /^\(\d{3}\)\d{3}.\d{4}$/;
 const zipPattern = /^\d{5}(-\d{4})$/;
-const namePattern = /^[a-zA-Z ]{2,30}$/;
-const statePattern = /^[A-Z]$/;
-const cityPattern = /^[A-Za-z]$/;
-
+const namePattern = /^[a-z,',-]+(\s)[a-z,',-]+$/i;
+const statePattern = /^[A-Z][A-Z]$/;
+const cityPattern = /^[a-zA-Z-\s]+$/;
 $(document).ready(() => {
     // setting values equal to elements on the form based on id
     const txtName = $('#name');
@@ -47,24 +46,23 @@ $(document).ready(() => {
        const name = txtName.val() ? txtName.val().trim() : ''
        const emailAddress = txtEmail.val() ? txtEmail.val().trim() : ''
        const phone = txtPhone.val() ? txtPhone.val().trim() : ''
-       const dateOfBirth =  txtDateOfBirth.val() ? txtDateOfBirth.val().trim : ''
+       const dateOfBirth =  txtDateOfBirth.val() ? txtDateOfBirth.val().trim() : ''
        const zip = txtZip.val() ? txtZip.val().trim() : ''
        const city = txtCity.val() ? txtCity.val().trim() : ''
        const state = txtState.val() ? txtState.val().trim() : ''
 
        //validating formats of all input
         !namePattern.test(name) ?
-           makeInvalid(txtName, 'Please enter First and Last name\n' +
-           'No numbers or special characters except hyphen (-)') :
-           makeValid(txtName);
+            makeInvalid(txtName, 'Please enter your first and last name.\n' +
+                'No special characters except for hyphen (-)') :
+            makeValid(txtName);
 
-        console.log(emailAddress)
        !emailPattern.test(emailAddress) ?
            makeInvalid(txtEmail, 'Please enter an email ending in .edu') :
            makeValid(txtEmail);
 
        !phonePattern.test(phone) ?
-           makeInvalid(txtPhone, 'Please enter a valid phone number. ex (999)999.9999') :
+           makeInvalid(txtPhone, 'Please enter a valid phone number. ex. (999)999.9999') :
            makeValid(txtPhone);
 
         !cityPattern.test(city) ?
@@ -79,7 +77,6 @@ $(document).ready(() => {
            makeInvalid(txtZip,'Please enter the zip code in the format 99999-9999.') :
            makeValid(txtZip);
 
-       console.log(dateOfBirth)
        if(!datePattern.test(dateOfBirth)) {
            makeInvalid(txtDateOfBirth, 'Please enter a date in the format MM/DD/YYYY.');
        } else if (Date.parse(dateOfBirth) > Date.now()) {
@@ -87,8 +84,25 @@ $(document).ready(() => {
        } else {
            makeValid(txtDateOfBirth);
        }
-        $('#results').text(isValid ? 'All fields contain valid entries.' : '');
 
+       if(isValid) {
+           const lab9info = []
+           //add values to the array
+           lab9info['name'] = name;
+           lab9info['email-address'] = emailAddress;
+           lab9info['phone-number'] = phone;
+           lab9info['city'] = city;
+           lab9info['state'] = state;
+           lab9info['zip-code'] = zip;
+           lab9info['birth-date'] = dateOfBirth;
+
+           sessionStorage.lab9info = '';
+           for (let i in lab9info) {
+               sessionStorage.lab9info += i + '=' + lab9info[i] + '|';
+           }
+           //redirect to webpage
+           location.href = 'profile.html';
+       }
        txtName.select().focus();
     });
 });
